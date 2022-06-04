126b64243f0f352b2d551f93dc4f4fb7081d489b
Lênin Colares <lenincolares@hotmail.com>
2022-06-04 04:12:42 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web MVC App from C# Course";
+            ViewData["Estudante"] = "Lênin Colares";
 
             return View();
         }
